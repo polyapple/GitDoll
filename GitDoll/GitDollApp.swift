207ec62499f8eea89901e2cc6edb4baf81e549e1
polyapple <polyappledev@gmail.com>
2023-06-03 20:42:07 +0900

    Upload base project

---
+++ GitDoll/GitDollApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDollApp.swift
+//  GitDoll
+//
+//  Created by polyappledev on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct GitDollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
